f8d63820166e68fb8c9567e2000eaaa1f87c6266
DR-Dev-Jack <114923262+DR-Dev-Jack@users.noreply.github.com>
2026-09-01 15:40:48 +0200

    a start
    a small start to the project for Remy so Remy can continue

---
+++ main.py
@@ -0,0 +1,15 @@
+# rocket setup
+rocket_weight = 250 # in grams
+rocket_drag_coefficient = 1 # yea this part is fucked
+rocket_motor_avg = 20 # in newton
+rocket_motor_max = 35 # in newton
+rocket_motor_brandtijd = 1 # in seconden
+
+zwaartekracht = 9.81 # in newtons
+
+def generate_moter_curve(power_avg, power_max, weight):
+
+def calculate_newton(name):
+    print("something")
+
+
