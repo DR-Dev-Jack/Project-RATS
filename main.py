af31fc0d02129d7377c70ca2d9f4138556936dc1
Remy van Tongeren <27454@edu.msa.nl>
2026-09-01 16:48:41 +0200

    pyplot
    (eigenlijk geen zak gedaan)

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
-# rocket setup
-rocket_weight = 250 # in grams
-rocket_drag_coefficient = 1 # yea this part is fucked
-rocket_motor_avg = 20 # in newton
-rocket_motor_max = 35 # in newton
-rocket_motor_brandtijd = 1 # in seconden
+import matplotlib.pyplot as plt
+import numpy as np
 
-zwaartekracht = 9.81 # in newtons
+# rocket setup
+rocket_weight = 0.25 # in kilogram
+rocket_drag_coefficient = 0.6 # to be defined
+rocket_motor_avg = 12 # in newton
+rocket_motor_max = 35 # in newton
+rocket_motor_brandtijd = 1.6 # in seconden
+
+valversnelling = 9.81 # in meter per seconde kwadraat
+zwaartekracht = valversnelling*rocket_weight
 
 def generate_moter_curve(power_avg, power_max, weight):
+    print(power_avg, power_max, weight)
+    xpoints = np.array([0, 10])
+    ypoints = np.array([0, 10])
 
-def calculate_newton(name):
+    plt.plot(xpoints, ypoints)
+    plt.show()
+
+
+def calculate_newton():
     print("something")
+
+calculate_newton()
+generate_moter_curve(rocket_motor_avg, rocket_motor_max, rocket_weight)
+
+
 
 
