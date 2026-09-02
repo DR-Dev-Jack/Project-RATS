0ef3f234bf3305ab4e1735d0331f979bd12388b2
Remy van Tongeren <27454@edu.msa.nl>
2026-09-02 11:39:30 +0200

    motorcurve
    de functie generatemotor curve is af

--- main.py
+++ main.py
@@ -11,10 +11,18 @@ rocket_motor_brandtijd = 1.6 # in seconden
 valversnelling = 9.81 # in meter per seconde kwadraat
 zwaartekracht = valversnelling*rocket_weight
 
-def generate_moter_curve(power_avg, power_max, weight):
-    print(power_avg, power_max, weight)
-    xpoints = np.array([0, 10])
-    ypoints = np.array([0, 10])
+def generate_moter_curve(power_max, brandtijd):
+    xcalc = []
+    ycalc = []
+    looprange = int(brandtijd * 10 + 1)
+    for i in range(0, looprange):
+        x = i/10
+        xcalc.append(x)
+        ycalc.append(-(power_max/(0.5*brandtijd)**2)*(x-(0.5*brandtijd))**2+power_max)
+
+
+    xpoints = np.array(xcalc)
+    ypoints = np.array(ycalc)
 
     plt.plot(xpoints, ypoints)
     plt.show()
@@ -24,7 +32,7 @@ def calculate_newton():
     print("something")
 
 calculate_newton()
-generate_moter_curve(rocket_motor_avg, rocket_motor_max, rocket_weight)
+generate_moter_curve(rocket_motor_max,rocket_motor_brandtijd)
 
 
 
