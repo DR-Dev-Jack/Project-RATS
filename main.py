30236f6098d589aa4f1955352856042fcfb63786
DR-Dev-Jack <114923262+DR-Dev-Jack@users.noreply.github.com>
2026-09-02 21:21:26 +0200

    fixes + fuel weight
    added fuel weight decreasing based on rocket motor force curve. Fixed air drag always pushing the rocket down, even when rocket momentum is going down. Cleaned physics to use normaal kracht instead of disabling gravity. And added commentary on what we still need to fix.

--- main.py
+++ main.py
@@ -4,19 +4,21 @@ import math
 
 # rocket setup
 rocket_weight = 0.25 # kilogram
+fuel_weight = 0.024 # still in kilograms
 rocket_drag_coefficient = 0.6 # to be defined
 rocket_motor_avg = 12 # newton
-rocket_motor_max = 35 # newton
+rocket_motor_max = 35 # still in newton
 rocket_motor_brandtijd = 1.6 # seconden
 rocket_surface = 4.42*10**-3 # vierkante meter
 
 valversnelling = 9.81 # meter per seconde kwadraat
-luchtdichtheid = 1.225 # kilogram per kubieke meter, bij 15C
+luchtdichtheid = 1.225 # kilogram per kubieke meter, bij 15C    !!!Add changing air density by height!!!
 
-zwaartekracht = valversnelling*rocket_weight
+#zwaartekracht = valversnelling*rocket_weight
+zwaartekracht = valversnelling
 k_waarde = .5*luchtdichtheid*rocket_surface*rocket_drag_coefficient
 
-def generate_moter_curve(brandtijd, d):
+def generate_moter_curve(brandtijd, d):  # !!!Average is 12 Ns, about 7N ): so not right!!!
     tcalc = [0, brandtijd/4, brandtijd]
     ncalc = [0, d*2, 0]
         
@@ -26,9 +28,9 @@ def generate_moter_curve(brandtijd, d):
     return xpoints, ypoints
 
 def calc_drag (k, speed):
-    return k*(speed**2)
+    return k * speed * abs(speed)
 
-def plot_height (x,y , mass, k, fz, dt=0.1):
+def plot_height (x, y , mass, k, valversnelling, d1, t1, dt=0.1):
     hcalc = []
     tcalc = []
 
@@ -36,9 +38,10 @@ def plot_height (x,y , mass, k, fz, dt=0.1):
     v = 0
     t = 0
     a = 0
-    fzw = 0
+    xy = 0
+    total = d1*t1
     while h >= 0:
-
+        # using RK4 would be better than this simple version of eulors function
         h += v*dt
 
         tcalc.append(t)
@@ -46,11 +49,14 @@ def plot_height (x,y , mass, k, fz, dt=0.1):
         
         fs = np.interp(t, x, y)
         fd = calc_drag(k, v)
+        xy += fs*dt
+        minus_weight = xy/total*0.024
+        fz = valversnelling*(mass - minus_weight)
+        fnorm = fz
+        if fs < fz:
+            fnorm = 0
 
-        if fs > fz:
-            fzw = fz
-
-        fn = fs - fzw - fd
+        fn = fs + fnorm - fz - fd
 
         if h <= 0:
             fn = fs
@@ -58,7 +64,6 @@ def plot_height (x,y , mass, k, fz, dt=0.1):
         a = fn / mass
         v += a*dt
 
-        
         t += dt
 
     xpoints = np.array(tcalc)
@@ -81,4 +86,4 @@ def plot_height (x,y , mass, k, fz, dt=0.1):
     plt.show()
 
 x_cords, y_cords = generate_moter_curve(rocket_motor_brandtijd, rocket_motor_avg)
-plot_height(x_cords, y_cords, rocket_weight, k_waarde,zwaartekracht)
+plot_height(x_cords, y_cords, rocket_weight, k_waarde,zwaartekracht, rocket_motor_avg, rocket_motor_brandtijd)
