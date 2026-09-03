345856a9fc287770869db2960c3417bf5f81bfe8
Remy van Tongeren <27454@edu.msa.nl>
2026-09-03 12:14:34 +0200

    luchtdichtheid
    als het goed is verandert nu de luchtdichtheid als je hoger komt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import math
 
 # rocket setup
 rocket_weight = 0.25 # kilogram
@@ -9,14 +10,15 @@ rocket_drag_coefficient = 0.6 # to be defined
 rocket_motor_avg = 12 # newton
 rocket_motor_max = 35 # still in newton
 rocket_motor_brandtijd = 1.6 # seconden
-rocket_surface = 4.42*10**-3 # vierkante meter
+rocket_surface = 4.42e-3 # vierkante meter
 
 valversnelling = 9.81 # meter per seconde kwadraat
-luchtdichtheid = 1.225 # kilogram per kubieke meter, bij 15C    !!!Add changing air density by height!!!
+# luchtdichtheid = 1.225 # kilogram per kubieke meter, bij 15C    !!!Add changing air density by height!!!
+temperatuur = 288.15 # kelvin, 15c
+gasconstante = 287.05 # joule per kilogram kelvin. in droge lucht, misschien aanpasbaar met luchtvochtigheid?
 
 #zwaartekracht = valversnelling*rocket_weight
-zwaartekracht = valversnelling
-k_waarde = .5*luchtdichtheid*rocket_surface*rocket_drag_coefficient
+# k_waarde = .5*luchtdichtheid*rocket_surface*rocket_drag_coefficient
 
 file_adres = 'TSP_D12.csv'
 skip_lines = 4
@@ -29,10 +31,21 @@ def generate_moter_curve(file_adres, skip_lines):  # !!!Average is 12 Ns, about 
 
     return time, thrust
 
+def calc_luchtdichtheid(R, T, h, g):
+    M = float(2.9*10**-2)
+    Po= 101325.0
+    presure_at_height = Po * math.e**((-1.0 * M*g*h)/(R*T))
+    luchtdichtheid = presure_at_height/(R*T)
+
+    return luchtdichtheid
+
+def calc_k (p, A, cd):
+    return .5*p*A*cd
+
 def calc_drag (k, speed):
     return k * speed * abs(speed)
 
-def plot_height (x, y , mass, k, g, d1, t1, dt=0.1):
+def plot_height (x, y , mass, g, d1, t1, R, T, A, cd, dt=0.1, ):
     hcalc = []
     tcalc = []
 
@@ -48,8 +61,10 @@ def plot_height (x, y , mass, k, g, d1, t1, dt=0.1):
 
         tcalc.append(t)
         hcalc.append(h)
-        
+
         fs = np.interp(t, x, y)
+        luchtdichtheid = calc_luchtdichtheid(R, T, h, g)
+        k = calc_k(luchtdichtheid, A, cd)
         fd = calc_drag(k, v)
         burned_weight += fs*dt
         minus_weight = burned_weight/total*0.024
@@ -89,4 +104,4 @@ def plot_height (x, y , mass, k, g, d1, t1, dt=0.1):
     plt.show()
 
 x_cords, y_cords = generate_moter_curve(file_adres, skip_lines)
-plot_height(x_cords, y_cords, rocket_weight, k_waarde,zwaartekracht, rocket_motor_avg, rocket_motor_brandtijd)
+plot_height(x_cords, y_cords, rocket_weight, valversnelling, rocket_motor_avg, rocket_motor_brandtijd, gasconstante, temperatuur, rocket_surface, rocket_drag_coefficient)
